0e7deb67c821c9185ca9f60d4ee0bc099b90940e
bohekafeiting <1363896952@qq.com>
2016-06-02 16:43:57 +0800

    Update separators.js

--- separators.js
+++ separators.js
@@ -8,5 +8,5 @@ function thousands_separators(num) {
   st=st.split('').reverse().join('').replace(/(\d{3})/g,'$1,').replace(/\,$/,'').split('').reverse().join('');
   return (st+num.substring(y));
 } 
-
+module.exports = thousands_separators;
 
